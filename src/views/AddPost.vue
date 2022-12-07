85d885b9f16b24dee9547df3f99c369529737c3a
belkisarslan <belkisarslan33@gmail.com>
2022-12-07 18:54:06 +0300

    addpost and post skeletal structure

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        <h1>Yeni bir yazı yayınla!</h1>
+        <div class="container px-5 my-5 px-5">
+                <div class="text-center mb-5">
+                <h2 class="fw-bolder">Haydi Vue Blog'u geliştirmemize sen de yardımcı ol!</h2>
+                <p class="lead mb-0">Yaz, gönder, yayınlayalım...</p>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-6">
+                        
+                        <form id="contactForm" >
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="title" type="text" placeholder="Başlık girin..."/>
+                                <label for="title">Başlık</label>
+                            </div>
+                            <!-- Nick Name input-->
+                            <div class="form-floating mb-3">
+                                 <input class="form-control" id="name" type="text" placeholder="Nick Name/ Takma İsim"/>
+                                <label for="name">Nick Name/Takma İsim</label>
+                            </div>
+    
+                            <!-- Message input-->
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem"></textarea>
+                                <label for="message">Message</label>
+                            </div>
+                           
+                            <!-- Submit Button-->
+                            <div class="d-grid"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 <script>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-        <h1>Yayınlanmış yazılar</h1>
+    <div class="py-5 border-bottom">
+        <div class="container px-5 my-5 px-5">
+                <div class="text-center mb-5">
+                    <h2 class="fw-bolder">Sizden Gelenler</h2>
+                    <p class="lead mb-0">Vue hakkında yorumlar, yenilikler, gelişmeler</p>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-6">
+                        <!-- deneme yazı-->
+                        <div class="card mb-4">
+                            <div class="card-body p-4">
+                                <div class="d-flex">
+                                    <div class="ms-4">
+                                       <h3>Vue composition</h3>
+                                        <p class="mb-1">Thank you for putting together such a great product. We loved working with you and the whole team, and we will be recommending you to others!</p>
+                                        <div class="small text-muted">- Nick Name, Location</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 <script>
