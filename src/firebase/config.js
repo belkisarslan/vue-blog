ef6b9a3f02bc68f0e6f1a99925d566c1d8bb464a
belkisarslan <belkisarslan33@gmail.com>
2022-12-08 19:08:37 +0300

    firebase installed

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.VUE_APP_ID
+};
+
+
+const fb = initializeApp(firebaseConfig);
+export {fb}
