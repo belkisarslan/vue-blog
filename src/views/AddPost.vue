d6847747633abd83baf066381435658f04987177
belkisarslan <belkisarslan33@gmail.com>
2022-12-13 15:38:26 +0300

    addpost actions

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -8,25 +8,25 @@
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6">
                         
-                        <form id="contactForm" >
+                        <form @submit.prevent="handleSubmit">
                             <div class="form-floating mb-3">
-                                <input class="form-control" id="title" type="text" placeholder="Başlık girin..."/>
+                                <input v-model="title" class="form-control" type="text" placeholder="Başlık girin..."/>
                                 <label for="title">Başlık</label>
                             </div>
                             <!-- Nick Name input-->
                             <div class="form-floating mb-3">
-                                 <input class="form-control" id="name" type="text" placeholder="Nick Name/ Takma İsim"/>
+                                 <input v-model="name" class="form-control" type="text" placeholder="Nick Name/ Takma İsim"/>
                                 <label for="name">Nick Name/Takma İsim</label>
                             </div>
     
                             <!-- Message input-->
                             <div class="form-floating mb-3">
-                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem"></textarea>
+                                <textarea v-model="message" class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem"></textarea>
                                 <label for="message">Message</label>
                             </div>
                            
                             <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                            <div class="d-grid"><button class="btn btn-primary btn-lg" id="submitButton" type="submit">Submit</button></div>
                         </form>
                     </div>
                 </div>
@@ -34,8 +34,28 @@
     </div>
 </template>
 <script>
+import { ref } from 'vue';
+import { fb } from '../firebase/config'
+import { addDoc, collection, getFirestore } from 'firebase/firestore/lite';
+ 
+export default {
+    setup(){
+        const title = ref();
+        const name = ref();
+        const message = ref();
 
+        function handleSubmit(){
+            const post = {
+            title: title.value,
+            name: name.value,
+            message: message.value
+        };
+        const db = getFirestore(fb)
+        const fbRef = collection(db,"posts")
+        addDoc(fbRef, post)
+        }
+       return {title, name, message, handleSubmit}
+        
+    }
+ }
 </script>
-<style>
-    
-</style>
